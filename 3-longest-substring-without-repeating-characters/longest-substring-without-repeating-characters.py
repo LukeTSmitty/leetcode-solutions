d8a15d3cf9b10ab00d8136a5e4e296651f85da5c
LukeTSmitty <luketsmith.2000@gmail.com>
2025-08-30 04:06:19 -0500

    Time: 938 ms (9.76%) | Memory: 12.1 MB (99.99%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,65 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        
+        proposed solution steps:
+        1. For every substring
+        2. check if substring is valid
+        3. update length of maxlen if valid
+        """
+        MAX_UNIQUE_SUBSTRING = 97
+        max_len = 0
+        characters = []
+        # go over all possible substring lengths
+        i = 0
+        longest = 0
+        
+        #limits
+        if len(s) > MAX_UNIQUE_SUBSTRING:
+            longest = MAX_UNIQUE_SUBSTRING
+        else:
+            longest = len(s)
+
+        while(len(characters) < MAX_UNIQUE_SUBSTRING and i < len(s)):
+            char = s[i]
+            i+=1
+            if (char not in characters):
+                characters.append(char)
+                max_len = 1
+        # print(characters)
+        for sub_len in range(longest, 0, -1):
+            index = 0
+            while (index + sub_len <= len(s)):
+                substring = s[index:sub_len+index]
+                index +=1
+    
+                # print(substring)
+                # Check if substring is valid
+                flag = 0
+                for element in characters:
+                    # print(substring)
+
+                    # print(substring.count(element))
+                    if (substring.count(element) > 1):
+                        flag = 1
+                        break
+                if (flag == 0):
+                    #if it's valid update max length and check for maximum
+                    # print("MADE IT")
+                    max_len = len(substring)
+                    return max_len
+        return max_len
+                
+            
+        
+
+
+
+            
+            
+            
+
+            
+        
