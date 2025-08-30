eda3e3816d08b4a26c593db2459be9a9e0bdbc49
LukeTSmitty <luketsmith.2000@gmail.com>
2025-08-30 15:08:40 -0500

    Time: 362 ms (13.88%) | Memory: 12.5 MB (96.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -12,7 +12,7 @@ class Solution(object):
         MAX_UNIQUE_SUBSTRING = 97
         max_len = 0
         characters = []
-        # go over all possible substring lengths
+        # go over all possible substring lengths starting with the largest
         i = 0
         longest = 0
         
@@ -22,29 +22,22 @@ class Solution(object):
         else:
             longest = len(s)
 
-        while(len(characters) < MAX_UNIQUE_SUBSTRING and i < len(s)):
-            char = s[i]
-            i+=1
-            if (char not in characters):
-                characters.append(char)
-                max_len = 1
-
         for sub_len in range(longest, 0, -1):
             index = 0
             while (index + sub_len <= len(s)):
                 substring = s[index:sub_len+index]
                 index +=1
-
-                # Check if substring is valid
                 flag = 0
-                for element in characters:
-                    if (substring.count(element) > 1):
+                # Check if substring is valid
+                for element in substring:
+                    if(substring.count(element)>1):
                         flag = 1
                         break
-                if (flag == 0):
-                    #if it's valid update max length and check for maximum
+                if flag == 0:
                     max_len = len(substring)
                     return max_len
+
+                
         return max_len
                 
             
